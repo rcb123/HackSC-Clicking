5551be05e0ab87298e28b587226c046716f055a8
Reza Banankhah <banankha@usc.edu>
2023-02-03 22:51:14 -0800

    Installed TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import path from 'path';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$root: path.resolve('./src'),
+			$components: path.resolve('./src/components'),
+			$static: path.resolve('/'),
+			$images: path.resolve('/images'),
+			$lib: path.resolve('/lib')
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
