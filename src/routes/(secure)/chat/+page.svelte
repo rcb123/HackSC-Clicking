2fb10789ea43ac641eef9899bd581afab45a3cf2
Reza Banankhah <banankha@usc.edu>
2023-02-04 22:49:25 -0800

    Merge liked page into match page

--- src/routes/(secure)/chat/+page.svelte
+++ src/routes/(secure)/chat/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="pt-[5vh]">
-	<div class="relative h-[80vh] w-[90%] bg-gray-100 flex flex-col mx-auto rounded-xl p-8">
+	<div class="relative h-[80vh] w-[90%] bg-gray-50 flex flex-col mx-auto rounded-xl p-8">
 		<ul class="flex flex-col-reverse flex-1 overflow-y-auto">
 			<Messages />
 		</ul>
