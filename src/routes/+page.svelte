2f00226d734cf008ca16b0a8b17d2eafea020d5e
Nicholas Schumacher <naschuma@usc.edu>
2023-02-04 20:15:03 -0800

    Updated homepage buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,11 @@
 	import { page } from '$app/stores';
 </script>
 
+
 <div class="flex h-[90vh] bg-slate-200">
+	<img class="absolute max-w-md" src="./vector_1.png" alt=""/>
+	<img class="absolute max-w-lg right-0 bottom-10 rotate-180" src="./vector_1.png" alt=""/>
+
 	<div class="bg-white m-auto w-[75vw] max-w-2xl shadow-xl rounded-2xl px-10 py-10 overflow-auto">
 		<h1 class="text-4xl tracking-wider font-bold">Clicking 🍿</h1>
 		<h2 class="text-2xl mb-[3%] tracking-wide font-semibold">Find your movie soulmate</h2>
@@ -20,14 +24,14 @@
 		<div class="mt-6">
 			{#if !$page.data.session}
 				<a href="/signup">
-					<button class="btn btn-primary rounded-3xl normal-case font-semibold text-base">Sign up</button>
+					<button class="btn bg-yellow-200 btn-primary rounded-3xl normal-case font-semibold text-base text-black border-none hover:bg-yellow-100">Sign up</button>
 				</a>
 				<a href="/login">
-					<button class="btn btn-primary rounded-3xl normal-case font-semibold text-base">Login</button>
+					<button class="btn bg-yellow-200 btn-primary rounded-3xl normal-case font-semibold text-base text-black border-none hover:bg-yellow-100">Login</button>
 				</a>
 			{:else}
 				<a href="/match">
-					<button class="btn btn-primary rounded-3xl normal-case font-semibold text-base">Start matching</button>
+					<button class="btn bg-yellow-200 btn-primary rounded-3xl normal-case font-semibold text-base text-black border-none hover:bg-yellow-100">Start matching</button>
 				</a>
 			{/if}
 		</div>
